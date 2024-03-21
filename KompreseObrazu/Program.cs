d53ac81f28ea7b003fe41b178d7173cbd6692ada
Filip Vavrečka <filip.vavrecka@student.vitkovickastredni.cz>
2024-03-21 13:54:44 +0100

    CV-pocet unikatnich barev

--- KompreseObrazu/Program.cs
+++ KompreseObrazu/Program.cs
@@ -1,29 +1,38 @@
-﻿namespace Komprese
-{
+﻿namespace Komprese{
     public class KompreseObrazu{
 
         public static void Main (String [] args){
         
+        String testFilePath = @"C:\Users\FilipVavrečka\github-classroom\Vitkovicka-stredni-prumyslova-skola\cv-komprese-obrazu-v2-FilipVavrecka\KompreseObrazu\CSV\obr1-10.csv";
         
-
-        //Cesta k testovacímu souboru
-        String testFilePath = @"C:\Users\MartinKapcala\OneDrive - Vítkovická střední průmyslová škola\Dokumenty\Programování\PG\Komprese-obrazu\KompreseObrazu\CSV\obr1-10.csv";
-        
-        //vytvoření instance třídy Obrazek
         Obrazek inputCSV = new Obrazek(testFilePath);
 
+        Console.WriteLine("Počet vertikálních řádků: {0}",inputCSV.CountLines(testFilePath));
+
+        Console.WriteLine("Počet horizontální řádků: {0}",inputCSV.CountSymbolInLine(testFilePath));
         
-        
-        //Test metody, která spočítá počet řádků vstupního obrázku
-        Console.WriteLine("Počet vertikálních řádků {0}",inputCSV.CountLines(testFilePath));
-        
-        //Test metody, která spočítá počet symbolů vstupního obrázku v prvním řádku
-        Console.WriteLine("Počet horizontální řádků {0}",inputCSV.CountSymbolInLine(testFilePath));
+        Console.WriteLine("----------------------------------------------");
 
         inputCSV.vypisImg();
-    }
+        
+        List<int> unikatniBarvy = inputCSV.PaletaBarevObrazku();
 
+        Console.WriteLine("----------------------------------------------");
+
+        Console.Write("Unikátní barvy v obrázku: ");
+        foreach (int element in unikatniBarvy){
+            Console.Write($"{element}, ");
+            }
+        
+        Console.WriteLine();
+        Console.WriteLine("----------------------------------------------");
+
+        Console.WriteLine("Počet výskytů jednotlivých barev:");
+        Dictionary<int, int> colorCounts = inputCSV.CountColors();
+        foreach (var colorCount in colorCounts)
+        {
+            Console.WriteLine($"Barva {colorCount.Key}: {colorCount.Value}x");
+            }
+        }
     }
-    
-    
 }
